c3c634baf5f45342589c5017d1384c759dfa287d
Clegineide <clegineidesilva7@gmail.com>
2023-08-13 21:07:58 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+        //if (html.classList.contains("light")) {
+        //html.classList.remove("light")
+        //} else {
+        //  htmlclassList.add('light')
+        //}
+        // Pegar a tag img
+
+    // Substituir a imagem
+    // se tiver a tag light mode, adicionar a imagem light
+    // SE tiver sem a lightmode, manter a imagem normal
+}
